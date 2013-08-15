f6558590c60dd5726f1b46bc6047e37ed6ce9c46
Erik Straub <erik@madgloryint.com>
2013-08-15 15:58:27 -0400

    Added file_name method for DataNotification. Bumped to 0.0.8

--- lib/device_cloud/push_notification/data_notification.rb
+++ lib/device_cloud/push_notification/data_notification.rb
@@ -2,6 +2,12 @@ module DeviceCloud
   class PushNotification::DataNotification < PushNotification::BaseNotification
     def initialize(file_data)
       @file_data = file_data
+      @id = data['id']
+    end
+
+    def file_name
+      return '' unless id
+      id['fdName']
     end
 
     def handle!
--- spec/device_cloud/push_notification/data_notification_spec.rb
+++ spec/device_cloud/push_notification/data_notification_spec.rb
@@ -1,19 +1,23 @@
 require 'spec_helper'
 
 describe DeviceCloud::PushNotification::DataNotification do
-  describe "#handle!" do
-    let(:data) do
-      {
-        'id' => 1234,
-        'device_id' => 'm:1392301029',
-        'type' => 'some type',
-        'queued_dt' => '2013-06-24T14:52:55.421Z',
-        'value' => {'this' => 'is a value'}
-      }
-    end
-    let(:file_data) { OpenStruct.new(data: data, full_path: '/foo/bar/baz.json') }
+  let(:data) do
+    {
+      'id' => {
+        'fdPath' => '/db/MadGlory/Meter/data/',
+        'fdName' => 'foobar_1.jpg'
+      },
+      'device_id' => 'm:1392301029',
+      'type' => 'some type',
+      'queued_dt' => '2013-06-24T14:52:55.421Z',
+      'value' => {'this' => 'is a value'}
+    }
+  end
+  let(:file_data) { OpenStruct.new(data: data, full_path: '/foo/bar/baz.json') }
 
-    subject { DeviceCloud::PushNotification::DataNotification.new file_data }
+  subject { DeviceCloud::PushNotification::DataNotification.new file_data }
+  
+  describe "#handle!" do
 
     it "should call the DeviceCloud.data_notification_handler with self" do
       handled_data = nil
@@ -21,6 +25,19 @@ describe DeviceCloud::PushNotification::DataNotification do
 
       subject.handle!
       expect(handled_data).to eq subject
+    end
+  end
+
+  describe "#file_name" do
+    context 'when id is present' do
+      its(:file_name) { should eq data['id']['fdName']}
+    end
+
+    context 'when id is nil' do
+      before(:each) do
+        data['id'] = nil
+      end
+      its(:file_name) { should eq '' }
     end
   end
 end
