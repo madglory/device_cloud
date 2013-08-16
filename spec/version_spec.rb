86bf09f10a4833193041565a57fa3cda17f2e8eb
Erik Straub <erik@madgloryint.com>
2013-08-16 10:22:27 -0400

    Bumped to 0.1.0

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'DeviceCloud::VERSION' do
   it 'should be the correct version' do
-    DeviceCloud::VERSION.should == '0.0.11'
+    DeviceCloud::VERSION.should == '0.1.0'
   end
 end
