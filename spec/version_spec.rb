b488e4f4450969b54ecda9a4ab0df28fa6b2d947
Erik Straub <erik@madgloryint.com>
2013-10-23 18:53:22 -0400

    Bumped version

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'DeviceCloud::VERSION' do
   it 'should be the correct version' do
-    DeviceCloud::VERSION.should == '0.2.2'
+    DeviceCloud::VERSION.should == '0.2.3'
   end
 end
