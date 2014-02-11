dda37a015269a9ccd8bfe4c0c821a2fdad3925ba
Erik Straub <erik@fiveeighteen.net>
2014-02-10 21:26:49 -0500

    Added webmock, pry, and awesome_print to spec_helper.rb Added idigi_username and idigi_password

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,24 @@
 require 'device_cloud'
 require 'ostruct'
+require 'webmock/rspec'
+require 'pry'
+require 'awesome_print'
+
+def idigi_username
+  'foouser'
+end
+
+def idigi_password
+  'barpass'
+end
 
 RSpec.configure do |config|
   config.mock_with :rspec
 
   config.before(:each) do
     DeviceCloud.configure do |configuration|
-      configuration.username = 'a username'
-      configuration.password = 'a password'
+      configuration.username = idigi_username
+      configuration.password = idigi_password
     end
   end
 end
